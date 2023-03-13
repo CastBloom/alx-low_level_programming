f68169a857d50799d3718646f456fc49c7b5d998
CastBloom <123015885+CastBloom@users.noreply.github.com>
2023-03-14 01:15:26 +0200

    Create 101-strtow.c
    commit 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+- Prototype: char **strtow(char *str);
+		
+- The function returns a pointer to an array of strings (words)
+		
+- Each element of this array should contain a single word, null-terminated
+		
+- The last element of the returned array should be NULL
+		
+- Words are separated by spaces
+		
+- Returns NULL if str == NULL or str == ""
+		
+- If your function fails, it should return NULL

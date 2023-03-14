154140c601837bcbec153a781ead27fbd0b934c5
CastBloom <123015885+CastBloom@users.noreply.github.com>
2023-03-14 21:20:13 +0200

    Delete 0x0B-malloc_free directory
    Commit changes

--- 0x0B-malloc_free/0-create_array.c
+++
@@ -1,9 +0,0 @@
-- Prototype: char *create_array(unsigned int size, char c);
-
-                
-
-- Returns NULL if size = 0
-
-                
-
-- Returns a pointer to the array, or NULL if it fails
--- 0x0B-malloc_free/4-free_grid.c
+++
@@ -1,5 +0,0 @@
-Prototype: void free_grid(int **grid, int height);
-
-                
-
-Note that we will compile with your alloc-grid.c file. Make sure it compiles.
